d1be6c34580710bf753b87fc7df4a494231cd990
idspittal <isaacdspittal@gmail.com>
2024-02-15 13:24:54 -0500

    Conditional Lighting works!
    LED lights up when object within 18cm

---
+++ Proof of Concept Test Code/Conditional_Light_Experiment/Conditional_Light_Experiment.ino
@@ -0,0 +1,35 @@
+/*
+ * HC-SR04 example sketch
+ */
+
+const int trigPin = 9;
+const int echoPin = 10;
+
+float duration, distance;
+
+void setup() {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration*.0343)/2;
+  if(distance < 18)
+    digitalWrite(LED_BUILTIN, HIGH);
+  else
+    digitalWrite(LED_BUILTIN, LOW);
+  Serial.print("Distance: ");
+  Serial.println(distance);
+  delay(100);
+}
